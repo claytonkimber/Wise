f9270c39b027903e05f14c0186e18d714455fa4c
claytonkimber <clayton.kimber.ie@gmail.com>
2026-03-04 18:24:15 +0000

    updated gitignore to exclude tester python files, and removed python files

--- patch_dnd_fix.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open("modules/DragAndDrop.lua", "r") as f:
-    content = f.read()
-
-# I already modified Wise:OnDragReceive earlier but let's just make sure it's correct.
-print(content[:1500])
